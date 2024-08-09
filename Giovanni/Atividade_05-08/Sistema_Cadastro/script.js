b8707d04d2d7ca92c7f8122153e09a1269f48c48
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-08-09 17:02:09 -0300

    atividade finalizada

--- Giovanni/Atividade_05-08/Sistema_Cadastro/script.js
+++ Giovanni/Atividade_05-08/Sistema_Cadastro/script.js
@@ -6,11 +6,8 @@ const phone = document.querySelector('#telefone');
 
 const btn = document.querySelector('.button-salvar');
 
-const card_delete = document.querySelector('.list-card');
-
 const lista_cadastro = document.querySelector('.list');
 
-var id_list = 0;
 
 btn.addEventListener('click', (event) => {
 
@@ -26,30 +23,25 @@ btn.addEventListener('click', (event) => {
 
     // Validações here
 
+    if (!cadastro_nome || !cadastro_email || !cadastro_telefone) {
+        alert("Campos em branco!");
+    } else {
+        const templeteHTML = `
+        <li class="list-card">
+            <div class="card">
+                <h2 class="nome">${cadastro_nome}</h2>
+                <h3 class="email">${cadastro_email}</h3>
+                <h3 class="Telefone">${cadastro_telefone}</h3>
+            </div>
+            <button class="del"> Excluir cadastro </button>
+        </li>`;
 
-    //
-
-    const templeteHTML = `
-             <li class="list-card" id="${id_list}">
-                 <div class="card">
-                     <h2 class="nome">${cadastro_nome}</h2>
-                     <h3 class="email">${cadastro_email}</h3>
-                     <h3 class="Telefone">${cadastro_telefone}</h3>
-                 </div>
-                 <button class="delete-button"> Excluir cadastro </button>
-             </li>
-     `;
-
-    lista_cadastro.innerHTML += templeteHTML;
-
-    // carregar no html here
-
-    console.log();
+        lista_cadastro.innerHTML += templeteHTML;
+    }
 
     funcDeletar();
 
     limpar_campos();
-    id_list++;
 });
 
 
@@ -62,59 +54,15 @@ function limpar_campos() {
 };
 
 
-const btnDelete = document.getElementsByClassName('delete-button')
-
 async function funcDeletar() {
 
-    for (let index = 0; index < btnDelete.length; index++) {
-        btnDelete[index].addEventListener('click', (e) => {
-            e.preventDefault();
-
-            btnDelete[index].parentElement.remove();
-
+    const deletar = document.querySelectorAll(".del");
+    deletar.forEach((btnDeletar) => {
+        // Ouvindo o evento no botao de deletar
+        btnDeletar.addEventListener("click", (e) => {
+            // Removendo a div pai do botão
+            btnDeletar.parentElement.remove();
+            console.log("Usuario Deletado");
         });
-    };
-
-};
-
-// function carregar_html() {
-
-//     lista_cadastro.innerHTML = '';
-
-//     for (let index = 0; index < array.length; index++) {
-//         const cadastro_salvo = array[index];
-
-//         const templeteHTML = `
-//              <li class="list-card" id="${cadastro_salvo.id}">
-//                  <div class="card">
-//                      <h2 class="nome">${cadastro_salvo.nome}</h2>
-//                      <h3 class="email">${cadastro_salvo.email}</h3>
-//                      <h3 class="Telefone">${cadastro_salvo.telefone}</h3>
-//                  </div>
-//                  <button class="delete-button"> Excluir cadastro </button>
-//              </li>
-//      `;
-
-//         lista_cadastro.innerHTML += templeteHTML;
-//     };
-// }
-
-/*btn_Excluir.addEventListener('click', (e) => {
-
-    btn_Excluir.style.visibility = 'hidden';
-
-    lista_cadastro.innerHTML = ''
-    //location.reload(); Função para recarregar a página
-});*/
-
-/*
-    lista_cadastro.querySelector('.delete-button').addEventListener('click', () => {
- 
-        const cadastro_deletado = document.querySelector('.delete-button').id;
- 
-        console.log(cadastro_deletado);
- 
- 
-        cadastro_deletado.remove(); // Remove o item da lista
     });
-*/
+}
