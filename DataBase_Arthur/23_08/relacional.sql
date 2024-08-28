c1c876245b847b5b725ab8972f91a363068c34f1
Raphael Cardoso <raphaelbcjr@gmail.com>
2024-08-28 16:18:25 -0300

    Add new class Flutter

--- DataBase_Arthur/23_08/relacional.sql
+++ DataBase_Arthur/23_08/relacional.sql
@@ -48,3 +48,37 @@ FOREIGN KEY (idFuncionario) REFERENCES funcionarios(id),
 FOREIGN KEY (idFornecedor) REFERENCES fornecedores(id)
 );
 
+INSERT INTO clientes (nome, idade) VALUES
+('DON JUAN', 27),
+('GEIBISLANE', 46),
+('JORGOSSO', 50);
+
+INSERT INTO produtos (nome, preco) VALUES
+('RUAN', 1),
+('ARROZ', 25),
+('PAO', 5);
+
+INSERT INTO fornecedores (nome, cnpj) VALUES
+('BETarthur', 0000000001),
+('amil', 4646546541),
+('exercito', 546546541);
+
+INSERT INTO funcionarios (nome, cpf, ncarteira, salario) VALUES
+('RUAN', 274654645, 12345678, 1),
+('VIVI', 46546544, 1231654, 1),
+('JAO', 50546546, 2165843, 1);
+
+SELECT * FROM funcionarios;
+SELECT * FROM clientes;
+SELECT * FROM produtos;
+SELECT * FROM fornecedores;
+
+INSERT INTO pedidos (idCliente, idProduto, idFuncionario, idFornecedor) VALUES
+(2, 1, 3, 3),
+(1, 3, 2, 1),
+(3, 2, 1, 2);
+
+SELECT * FROM pedidos;
+
+
+
